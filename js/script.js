31ca74205f0a1f82a2b7656f238897aed186a3a3
Ebubekir D. <ebudemirkazik@gmail.com>
2025-04-20 10:59:10 +0300

    f1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// 1. Gerekli öğeleri seçiyoruz
+const lengthInput = document.getElementById("length");
+const uppercaseCheckbox = document.getElementById("uppercase");
+const lowercaseCheckbox = document.getElementById("lowercase");
+const numbersCheckbox = document.getElementById("numbers");
+const symbolsCheckbox = document.getElementById("symbols");
+const generateButton = document.getElementById("generate");
+const resultDiv = document.getElementById("result");
+
